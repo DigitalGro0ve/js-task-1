f20a84e5105c66e4259bd3a1fee9d217af173bb9
DigitalGro0ve <vtorc17@freeuni.edu.ge>
2022-10-21 17:16:19 +0400

    js task 1

---
+++ script.js
@@ -0,0 +1,28 @@
+//var income = prompt();
+//if (income<= 1500){
+  //  alert("საშუალოზე დაბალი")
+//}
+//else if (income>1500 && income <= 2000){
+  //  alert("საშუალო")
+//}
+//else {
+  //  alert("საშუალოზე მაღალი")
+//} 
+
+// var numbers = [1,4,18,24,15,2,3,19,55];
+// var bigOne = numbers [0];
+// for (var i=0; i<numbers.length; i++){
+//     if (bigOne < numbers[i]){
+//         bigOne = numbers[i];
+//     }
+// }
+// console.log(bigOne);
+
+// var numbers = [1,4,18,24,15,2,3,19,55];
+// var smallOne = numbers[0];
+// for (var i=0; i<numbers.length; i++){
+//     if(smallOne > numbers[i]){
+//         smallOne = numbers[i];
+//     }
+// }
+// console.log(smallOne);
